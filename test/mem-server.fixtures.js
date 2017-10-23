d138883e102c8833b3709a43070ff8b744860c3b
Izel Nakri <contact@izelnakri.com>
2017-10-23 22:57:51 +0200

    mem-server.fixtures and men-server.start tests complete

--- test/mem-server.fixtures.js
+++ test/mem-server.fixtures.js
@@ -1,12 +1,9 @@
 const assert = require('assert');
 const fs = require('fs');
 const rimraf = require('rimraf');
-const sinon = require('sinon');
 
-describe('MemServer start/stop functionality', function() {
+describe('MemServer fixture constraint feature', function() {
   beforeEach(function() {
-    this.timeout(5000);
-
     const modelFileContent = `import Model from '${process.cwd()}/lib/mem-server/model';
 
     export default Model({});`;
@@ -22,27 +19,18 @@ describe('MemServer start/stop functionality', function() {
   });
 
   afterEach(function(done) {
+    Object.keys(require.cache).forEach((key) => delete require.cache[key]);
+
     if (fs.existsSync(`${process.cwd()}/memserver`)) {
       rimraf.sync(`${process.cwd()}/memserver`);
     }
+
     done();
   });
 
-  it('can be started without fixtures', function() {
-    const MemServer = require('../index.js');
-
-    assert.deepEqual(MemServer.Pretender, {});
-
-    MemServer.start();
-
-    assert.deepEqual(Object.keys(MemServer.Pretender), [
-      'hosts', 'handlers', 'handledRequests', 'passthroughRequests', 'unhandledRequests',
-      'requestReferences', 'forcePassthrough', 'disableUnhandled', '_nativeXMLHttpRequest',
-      'running', 'handledRequest', 'passthroughRequest', 'unhandledRequest'
-    ]);
-  });
-
-  it('can be started with fixtures', function() {
+  it('should throw error if any of the fixtures missing id or uuid', function() {
+    this.timeout(5000);
+    
     fs.mkdirSync(`./memserver/fixtures`);
     fs.writeFileSync(`${process.cwd()}/memserver/fixtures/photos.js`, `export default [
       {
@@ -53,6 +41,72 @@ describe('MemServer start/stop functionality', function() {
       },
       {
         id: 2,
+        name: 'Family photo',
+        href: 'family-photo.jpeg',
+        is_public: true
+      },
+      {
+        id: 3,
+        name: 'Selfie',
+        href: 'selfie.jpeg',
+        is_public: false
+      }
+    ];`);
+
+    fs.writeFileSync(`${process.cwd()}/memserver/fixtures/photo-comments.js`, `export default [
+      {
+        content: 'What a nice photo!',
+        photo_id: 1,
+        user_id: 1
+      },
+      {
+        content: 'I agree',
+        photo_id: 1,
+        user_id: 2
+      },
+      {
+        content: 'I was kidding',
+        photo_id: 1,
+        user_id: 1
+      },
+      {
+        content: 'Interesting indeed',
+        photo_id: 2,
+        user_id: 1
+      }
+    ];`);
+
+    const MemServer = require('../index.js');
+    const { Photo, PhotoComment } = MemServer.Models;
+
+    assert.deepEqual(MemServer.Pretender, {});
+    assert.deepEqual(MemServer.DB, {});
+
+    assert.deepEqual(Photo.findAll(), []);
+    assert.deepEqual(PhotoComment.findAll(), []);
+
+    assert.throws(() => MemServer.start(), (err) => {
+      return (err instanceof Error) &&
+        /MemServer DATABASE ERROR\: At least one of your PhotoComment fixtures missing a primary key\. Please make sure all your PhotoComment fixtures have either id or uuid primaryKey/.test(err);
+    });
+
+    assert.deepEqual(MemServer.Pretender, {});
+    assert.deepEqual(MemServer.DB, {});
+    assert.deepEqual(Photo.findAll(), []);
+    assert.deepEqual(PhotoComment.findAll(), []);
+  });
+
+  it('should throw error if any of the id fixtures have an incorrect type', function() {
+    fs.mkdirSync(`./memserver/fixtures`);
+    fs.writeFileSync(`${process.cwd()}/memserver/fixtures/photos.js`, `export default [
+      {
+        id: 1,
+        name: 'Ski trip',
+        href: 'ski-trip.jpeg',
+        is_public: false
+      },
+      {
+        id: '2',
         name: 'Family photo',
         href: 'family-photo.jpeg',
         is_public: true
@@ -101,50 +155,18 @@ describe('MemServer start/stop functionality', function() {
     assert.deepEqual(Photo.findAll(), []);
     assert.deepEqual(PhotoComment.findAll(), []);
 
-    MemServer.start();
-
-    assert.deepEqual(Object.keys(MemServer.Pretender), [
-      'hosts', 'handlers', 'handledRequests', 'passthroughRequests', 'unhandledRequests',
-      'requestReferences', 'forcePassthrough', 'disableUnhandled', '_nativeXMLHttpRequest',
-      'running', 'handledRequest', 'passthroughRequest', 'unhandledRequest'
-    ]);
-    assert.deepEqual(MemServer.DB, {
-      Photo: Photo.findAll(), PhotoComment: PhotoComment.findAll(), User: []
+    assert.throws(() => MemServer.start(), (err) => {
+      return (err instanceof Error) &&
+        /MemServer Photo model primaryKeyType is 'id'. Instead you've tried to enter id: 2 with string type/.test(err);
     });
-    assert.equal(Photo.primaryKey, 'id');
-    assert.deepEqual(Photo.findAll().length, 3);
-    assert.deepEqual(Photo.find(1), {
-      id: 1, name: 'Ski trip', href: 'ski-trip.jpeg', is_public: false
-    });
-
-    assert.equal(PhotoComment.primaryKey, 'uuid');
-    assert.deepEqual(PhotoComment.findAll().length, 4);
-    assert.deepEqual(PhotoComment.findBy({ uuid: '374c7f4a-85d6-429a-bf2a-0719525f5f29' }), {
-      uuid: '374c7f4a-85d6-429a-bf2a-0719525f5f29', content: 'Interesting indeed',
-      photo_id: 2, user_id: 1
-    });
-  });
-
-  it('can be shut down', () => {
-    const MemServer = require('../index.js');
 
     assert.deepEqual(MemServer.Pretender, {});
-
-    MemServer.start();
-
-    assert.deepEqual(Object.keys(MemServer.Pretender), [
-      'hosts', 'handlers', 'handledRequests', 'passthroughRequests', 'unhandledRequests',
-      'requestReferences', 'forcePassthrough', 'disableUnhandled', '_nativeXMLHttpRequest',
-      'running', 'handledRequest', 'passthroughRequest', 'unhandledRequest'
-    ]);
-
-    MemServer.Pretender.shutdown = sinon.spy();
-    MemServer.shutdown();
-
-    assert.ok(MemServer.Pretender.shutdown.calledOnce, 'MemServer.shutdown() calls shutdown on Pretender instance');
+    assert.deepEqual(MemServer.DB, {});
+    assert.deepEqual(Photo.findAll(), []);
+    assert.deepEqual(PhotoComment.findAll(), []);
   });
 
-  it('can be shut down and started again with restarted state', () => {
+  it('should throw error if any of the uuid fixtures have an incorrect type', function() {
     fs.mkdirSync(`./memserver/fixtures`);
     fs.writeFileSync(`${process.cwd()}/memserver/fixtures/photos.js`, `export default [
       {
@@ -187,7 +209,7 @@ describe('MemServer start/stop functionality', function() {
         user_id: 1
       },
       {
-        uuid: '374c7f4a-85d6-429a-bf2a-0719525f5f29',
+        uuid: 12,
         content: 'Interesting indeed',
         photo_id: 2,
         user_id: 1
@@ -198,33 +220,19 @@ describe('MemServer start/stop functionality', function() {
     const { Photo, PhotoComment } = MemServer.Models;
 
     assert.deepEqual(MemServer.Pretender, {});
+    assert.deepEqual(MemServer.DB, {});
 
-    MemServer.start();
+    assert.deepEqual(Photo.findAll(), []);
+    assert.deepEqual(PhotoComment.findAll(), []);
 
-    assert.deepEqual(Object.keys(MemServer.Pretender), [
-      'hosts', 'handlers', 'handledRequests', 'passthroughRequests', 'unhandledRequests',
-      'requestReferences', 'forcePassthrough', 'disableUnhandled', '_nativeXMLHttpRequest',
-      'running', 'handledRequest', 'passthroughRequest', 'unhandledRequest'
-    ]);
+    assert.throws(() => MemServer.start(), (err) => {
+      return (err instanceof Error) &&
+        /MemServer PhotoComment model primaryKeyType is 'uuid'. Instead you've tried to enter uuid: 12 with number type/.test(err);
+    });
 
-    const initialPhotos = Photo.findAll();
-    const initialPhotoComments = PhotoComment.findAll();
-
-    assert.equal(initialPhotos.length, 3);
-    assert.equal(initialPhotoComments.length, 4);
-
-    Photo.insert({ id: 55, name: 'Great another photo' });
-    PhotoComment.insert({ uuid: '21f5ef47-332a-4d68-91b8-b7e13aa2f941', content: 'interesting photo indeed' });
-    Photo.insert({ id: 56, name: 'one more' });
-    Photo.insert({ id: 57, name: 'another' });
-
-    assert.equal(Photo.findAll().length, 6);
-    assert.equal(PhotoComment.findAll().length, 5);
-
-    MemServer.shutdown();
-    MemServer.start();
-
-    assert.deepEqual(Photo.findAll(), initialPhotos);
-    assert.deepEqual(PhotoComment.findAll(), initialPhotoComments);
+    assert.deepEqual(MemServer.Pretender, {});
+    assert.deepEqual(MemServer.DB, {});
+    assert.deepEqual(Photo.findAll(), []);
+    assert.deepEqual(PhotoComment.findAll(), []);
   });
 });

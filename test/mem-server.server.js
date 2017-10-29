40f52b237f81e0f8c0bd1756e3a4e1523d31bf4b
Izel Nakri <contact@izelnakri.com>
2017-10-29 19:04:01 +0100

    initial working MemServer.Server. More tests coming soon.

--- test/mem-server.server.js
+++ test/mem-server.server.js
@@ -16,8 +16,8 @@ describe('MemServer.Server functionality', function() {
       import Model from '${process.cwd()}/lib/mem-server/model';
 
       export default Model({
-        findFromToken(request) {
-          const authorizationHeader = request.requestHeaders.Authorization;
+        findFromHeaderToken(headers) {
+          const authorizationHeader = headers.Authorization;
           const token = authorizationHeader ? authorizationHeader.slice(6) : null;
 
           return this.findBy({ authentication_token: token });
@@ -243,8 +243,8 @@ describe('MemServer.Server functionality', function() {
         import Response from '../lib/mem-server/response';
 
         export default function({ User, Photo }) {
-          this.post('/photos', (request) => {
-            const user = User.findFromToken(request);
+          this.post('/photos', ({ headers }) => {
+            const user = User.findFromHeaderToken(headers);
             console.log('user is', user);
 
             if (!user) {
@@ -253,11 +253,11 @@ describe('MemServer.Server functionality', function() {
 
             const photo = Photo.insert({ user_id: user.id });
 
-            return Photo.serialize(photo);
+            return { photo: Photo.serializer(photo) };
           });
 
-          this.get('/photos', (request) => {
-            const user = User.findFromToken(request);
+          this.get('/photos', ({ headers }) => {
+            const user = User.findFromHeaderToken(headers);
 
             if (!user) {
               return Response(404, { error: 'Not found' });
@@ -265,11 +265,11 @@ describe('MemServer.Server functionality', function() {
 
             const photos = Photo.findAll({ user_id: user.id });
 
-            return Photo.serialize(photos);
+            return { photos: Photo.serializer(photos) };
           });
 
-          this.get('/photos/:id', ({ params, headers }) => {
-            const user = User.findFromToken(request);
+          this.get('/photos/:id', ({ headers, params }) => {
+            const user = User.findFromHeaderToken(headers);
 
             if (!user) {
               return Response(401, { error: 'Unauthorized' });
@@ -277,22 +277,28 @@ describe('MemServer.Server functionality', function() {
 
             const photo = Photo.findBy({ id: params.id, user_id: user.id });
 
-            return photo ? Photo.serialize(photo) : Response(404, { error: 'Not found'})
+            return photo ? { photo: Photo.serializer(photo) } : Response(404, { error: 'Not found'});
           });
 
-          this.put('/photos/:id', ({ params, headers }) => {
-            const user = User.findFromToken(request);
+          this.put('/photos/:id', ({ headers, params }) => {
+            const user = User.findFromHeaderToken(headers);
 
-            if (user && Photo.findBy({ id: params.id, user_id: user.id })) {
-              return Photo.update(request.params);
+            if (!user) {
+              return Response(401, { error: 'Unauthorized' });
+            }
+
+            if (Photo.findBy({ id: params.id, user_id: user.id })) {
+              return { photo: Photo.update(params.photo) };
             }
           });
 
-          this.delete('/photos/:id', ({ params, headers }) => {
-            const user = User.findFromToken(request);
+          this.delete('/photos/:id', ({ headers, params }) => {
+            const user = User.findFromHeaderToken(headers);
+            console.log('called');
+            console.log(user);
 
             if (user && Photo.findBy({ id: params.id, user_id: user.id })) {
-              return Photo.delete(request.params); // NOTE: what to do with this response
+              return Photo.delete({ id: params.id });
             }
           });
         }
@@ -303,7 +309,7 @@ describe('MemServer.Server functionality', function() {
       this.timeout(5000);
 
       const MemServer = require('../index.js');
-      const { Photo, PhotoComment } = MemServer.Models;
+      const { Photo } = MemServer.Models;
 
       MemServer.start();
       window.$.ajaxSetup({ headers: { 'Content-Type': 'application/json' } });
@@ -319,13 +325,15 @@ describe('MemServer.Server functionality', function() {
         type: 'POST', url: '/photos', headers: AJAX_AUTHORIZATION_HEADERS
       }).then((data, textStatus, jqXHR) => {
         assert.equal(jqXHR.status, 201);
-        assert.deepEqual(data, { is_public: true, name: 'Some default name', id: 4, user_id: 1 });
+        assert.deepEqual(data, {
+          photo: { is_public: true, name: 'Some default name', id: 4, user_id: 1 }
+        });
       });
     });
 
     it('GET /resources works with custom headers and responses', async function() {
       const MemServer = require('../index.js');
-      const { Photo, PhotoComment } = MemServer.Models;
+      const { Photo } = MemServer.Models;
 
       MemServer.start();
 
@@ -336,7 +344,6 @@ describe('MemServer.Server functionality', function() {
         assert.deepEqual(jqXHR.responseJSON, { error: 'Not found' });
       });
 
-      // TODO: error here
       await window.$.ajax({
         type: 'GET', url: '/photos', headers: AJAX_AUTHORIZATION_HEADERS
       }).then((data, textStatus, jqXHR) => {
@@ -345,19 +352,76 @@ describe('MemServer.Server functionality', function() {
       });
     });
 
-    // it('GET /resources/:id works with custom headers and responses', function() {
-    //
-    // });
-    //
-    // it('PUT /resources/:id works with custom headers and responses', function() {
-    //
-    // });
-    //
-    // it('DELETE /resources/:id works with custom headers and responses', function() {
-    //
-    // });
-  // });
-});
+    it('GET /resources/:id works with custom headers and responses', async function() {
+      const MemServer = require('../index.js');
+      const { Photo } = MemServer.Models;
+
+      MemServer.start();
+
+      await window.$.ajax({
+        type: 'GET', url: '/photos/1', headers: { 'Content-Type': 'application/json' }
+      }).catch((jqXHR) => {
+        assert.equal(jqXHR.status, 401);
+        assert.deepEqual(jqXHR.responseJSON, { error: 'Unauthorized' });
+      });
+
+      await window.$.ajax({
+        type: 'GET', url: '/photos/1', headers: AJAX_AUTHORIZATION_HEADERS
+      }).then((data, textStatus, jqXHR) => {
+        assert.equal(jqXHR.status, 200);
+        assert.deepEqual(data, { photo: Photo.serializer(Photo.find(1))});
+      });
+    });
+
+    it('PUT /resources/:id works with custom headers and responses', async function() {
+      const MemServer = require('../index.js');
+      const { Photo } = MemServer.Models;
+
+      MemServer.start();
+
+      await window.$.ajax({
+        type: 'PUT', url: '/photos/1', headers: { 'Content-Type': 'application/json' },
+        data: JSON.stringify({ photo: { id: 1, name: 'Photo after edit' }})
+      }).catch((jqXHR) => {
+        assert.equal(jqXHR.status, 401);
+        assert.deepEqual(jqXHR.responseJSON, { error: 'Unauthorized' });
+      });
+
+      await window.$.ajax({
+        type: 'PUT', url: '/photos/1', headers: AJAX_AUTHORIZATION_HEADERS,
+        data: JSON.stringify({ photo: { id: 1, name: 'Photo after edit' } })
+      }).then((data, textStatus, jqXHR) => {
+        assert.equal(jqXHR.status, 200);
+        assert.deepEqual(data, { photo: Photo.serializer(Photo.find(1))});
+        assert.equal(Photo.find(1).name, 'Photo after edit');
+      });
+    });
+
+    it('DELETE /resources/:id works with custom headers and responses', async function() {
+      const MemServer = require('../index.js');
+      const { Photo } = MemServer.Models;
+
+      MemServer.start();
+
+      assert.ok(Photo.find(1), 'User id: 1 exists');
+
+      await window.$.ajax({
+        type: 'DELETE', url: '/photos/1', headers: { 'Content-Type': 'application/json' }
+      }).catch((jqXHR) => {
+        assert.equal(jqXHR.status, 401);
+        assert.deepEqual(jqXHR.responseJSON, { error: 'Unauthorized' });
+        assert.ok(Photo.find(1), 'User id: 1 exists');
+      });
+
+      await window.$.ajax({
+        type: 'DELETE', url: '/photos/1', headers: AJAX_AUTHORIZATION_HEADERS
+      }).then((data, textStatus, jqXHR) => {
+        assert.equal(jqXHR.status, 204);
+        assert.deepEqual(data, undefined);
+        assert.ok(!Photo.find(1), 'User id: 1 gets deleted');
+      });
+    });
+  });
 
   // describe('server can process custom queryParams and responses', function() {
   //   fs.writeFileSync(`${process.cwd()}/memserver/server.js`, `

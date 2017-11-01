5536421ece12b42ff6a6056869f1f3afdbf6b1bb
Izel Nakri <contact@izelnakri.com>
2017-11-01 23:23:55 +0100

    DONE: ES MODULE HACKING DONE FOR BROWSER

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,13 @@ require('babel-register')({
 });
 
 import fs from 'fs';
-import util from 'util';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import globals from 'rollup-plugin-node-globals';
 import builtins from 'rollup-plugin-node-builtins';
 import Inflector from 'i';
 import { classify, dasherize } from 'ember-cli-string-utils';
+import virtual from 'rollup-plugin-virtual';
 
 import JSToCode from './js-to-code';
 
@@ -22,41 +22,22 @@ if (!fs.existsSync(targetMemServerPath)) {
   throw new Error('memserver/server.js doesnt exist! Please create a memserver/server.js to use MemServer');
 }
 
-const targetMemServer = require(targetMemServerPath).default;
-const modelFixtureTree = modelFileNames.reduce((tree, fileName) => {
-  const modelName = fileName.slice(0, -3);
-  const fixturePath = `${process.cwd()}/memserver/fixtures/${dasherize(pluralize(modelName))}.js`;
-
-  return Object.assign(tree, {
-    [classify(modelName)]: {
-      model: require(`${process.cwd()}/memserver/models/${fileName}`).default,
-      fixtures: fs.existsSync(fixturePath) ? require(fixturePath).default : []
-    }
-  });
-}, {});
-const modelFixtureTreeCode = '{' + modelFileNames.reduce((string, modelFileName) => {
-  const modelName = classify(modelFileName.slice(0, -3));
-
-  return string + `${modelName}: {
-      model: ${JSToCode(modelFixtureTree[modelName].model)},
-      fixtures: ${util.inspect(modelFixtureTree[modelName].fixtures, { depth: null })}
-   }`
-}, '') + `}`;
-
-// TODO: INVESTIGATE ROLLUP: inject targetMemServer, modelFixtureTree from NODE -> BROWSER without dynamic import problem
-
 export default {
-  input: 'lib/mem-server.js',
+  input: 'lib/browser.js',
   output: {
-    file: 'dist/lol.js',
+    file: 'memserver.dist.js',
     format: 'iife'
   },
   name: 'MEMSERVER',
-  footer: `
-    const targetNamespace = typeof global === 'object' ? global : window;
-    window.MemServer = MEMSERVER(${modelFixtureTreeCode}, ${targetMemServer})
-  `,
+  footer: 'window.MemServer = MEMSERVER;',
   plugins: [
+    virtual({
+      '_memserver_models': generateInMemoryModelsImport(modelFileNames),
+      '_memserver_fixtures': generateInMemoryFixturesImport(modelFileNames),
+      '_memserver': `
+        import server from '${process.cwd()}/memserver/server';
+        export default server;`
+    }),
     resolve({ jsnext: true }),
     commonjs({
       include: 'node_modules/**',
@@ -66,6 +47,37 @@ export default {
       }
     }),
     globals(),
-    builtins()
+    builtins(),
   ]
-};
+}
+
+function generateInMemoryFixturesImport(modelFileNames) {
+  const imports = modelFileNames.reduce((codeText, modelFileName) => {
+    const modelName = classify(modelFileName.slice(0, -3));
+
+    return codeText + `import ${modelName} from '${process.cwd()}/memserver/fixtures/${dasherize(pluralize(modelName))}';`;
+  }, '');
+
+  return imports.concat(createObjectFromModels(modelFileNames));
+}
+function generateInMemoryModelsImport(modelFileNames) {
+  const imports = modelFileNames.reduce((codeText, modelFileName) => {
+    const modelName = classify(modelFileName.slice(0, -3));
+
+    return codeText + `import ${modelName} from '${process.cwd()}/memserver/models/${modelFileName}';`;
+  }, '');
+
+  return imports.concat(createObjectFromModels(modelFileNames));
+}
+
+function createObjectFromModels(modelFileNames) {
+  return 'export default { ' + modelFileNames.reduce((codeText, modelFileName) => {
+    const modelName = classify(modelFileName.slice(0, -3));
+
+    if (codeText === '') {
+      return modelName;
+    }
+
+    return [codeText, modelName].join(', ');
+  }, '') + ' };'
+}
